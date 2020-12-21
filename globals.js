cd98356be7242b30814c8b5dddae169bac7601f7
asafonov <me@asafonov.org>
2020-12-21 13:24:45 +0000

    account updated event

--- globals.js
+++ globals.js
@@ -2,6 +2,7 @@ window.asafonov = {};
 window.asafonov.utils = new Utils();
 window.asafonov.messageBus = new MessageBus();
 window.asafonov.events = {
+  ACCOUNT_UPDATED: 'accountUpdated',
 };
 window.asafonov.settings = {
 };
--- models/accounts.js
+++ models/accounts.js
@@ -1,2 +1,8 @@
 class Accounts extends AbstractList {
+
+  updateItem (id, item) {
+    asafonov.messageBus.send(asafonov.events.ACCOUNT_UPDATED, {id: id, from: {...this.list[id]}, to: item});
+    super.updateItem(id, item);
+  }
+
 }
