ce60cfb04c216a33b2eba04722ecc7b316500348
asafonov <me@asafonov.org>
2021-01-17 17:13:32 +0300

    report by tags

--- view/reports.js
+++ view/reports.js
@@ -2,13 +2,24 @@ class ReportsView {
 
   constructor() {
     this.model = new Reports();
-    this.circleLen = 30 * 0.42 * 2 * Math.pi;
-    this.circleElement = document.querySelector('.monly-circle .donut.chart svg');
-    this.totalElement = document.querySelector('.monly-circle .number.big');
+    this.circleLen = 30 * 0.42 * 2 * Math.PI;
+    this.element = document.querySelector('.monly-circle');
+    this.circleElement = this.element.querySelector('.donut.chart svg');
+    this.totalElement = this.element.querySelector('.number.big');
+    this.donutElement = this.element.querySelector('.donut.chart');
+  }
+
+  clearExistingItems() {
+    const items = this.element.querySelectorAll('.item');
+
+    for (let i = 0; i < items.length; ++i) {
+      this.element.removeChild(items[i]);
+    }
   }
 
   show() {
     this.circleElement.innerHTML = '';
+    this.clearExistingItems();
 
     this.model.build();
     const data = this.model.getItem(0);
@@ -24,9 +35,21 @@ class ReportsView {
       circle.className = `slice_${i}`;
       circle.style.strokeDasharray = `${lineLen} ${spaceLen}`;
       circle.style.strokeDashoffset = offset;
+      this.circleElement.innerHTML += circle.outerHTML;
+
+      const itemDiv = document.createElement('div');
+      itemDiv.className = 'item';
+      itemDiv.innerHTML = `<div><span class="bullet slice_${i}"></span>${item}</div>`;
+      const displayMoney = asafonov.utils.displayMoney(data[item]);
+      itemDiv.innerHTML += `<div class="number">${displayMoney}</div>`;
+      this.donutElement.after(itemDiv);
+
       offset -= lineLen;
       ++i;
-      this.circleElement.appendChild(circle);
     }
+
+    const circle = document.createElement('circle');
+    circle.className = 'slice_f';
+    this.circleElement.innerHTML += circle.outerHTML;
   }
 }
