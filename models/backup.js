443a26ecd9463549a49ff0a5cf38ebcd760024ea
asafonov <me@asafonov.org>
2022-03-16 15:46:13 +0300

    backup

--- models/backup.js
+++ models/backup.js
@@ -3,6 +3,24 @@ class Backup {
     window.localStorage.clear()
   }
 
+  backup (hostname) {
+    const list = {...window.localStorage}
+
+    fetch('http://' + hostname + ':9092/monly', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(list)
+    })
+    .then(() => {
+      alert("Backup completed");
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+  }
+
   destroy() {
   }
 }
--- view/backup.js
+++ view/backup.js
@@ -4,7 +4,9 @@ class BackupView {
     this.model = new Backup()
     this.mainContainer = document.querySelector('.settings-backup')
     this.clearButton = this.mainContainer.querySelector('.clear')
+    this.backupButton = this.mainContainer.querySelector('.backup')
     this.onClearButtonClickedProxy = this.onClearButtonClicked.bind(this)
+    this.onBackupButtonClickedProxy = this.onBackupButtonClicked.bind(this)
     this.addEventListeners()
   }
 
@@ -18,12 +20,22 @@ class BackupView {
 
   updateEventListeners (add) {
     this.clearButton[add ? 'addEventListener' : 'removeEventListener']('click', this.onClearButtonClickedProxy)
+    this.backupButton[add ? 'addEventListener' : 'removeEventListener']('click', this.onBackupButtonClickedProxy)
   }
 
   onClearButtonClicked() {
     if (confirm('Are you sure you want to clear App data? This can\'t be undone')) {
       this.model.clear()
       location.reload()
+    }
+  }
+
+  onBackupButtonClicked() {
+    const hostname = prompt('Hostname', window.localStorage.getItem('hostname') || '192.168.0.1')
+
+    if (hostname) {
+      window.localStorage.setItem('hostname', hostname)
+      this.model.backup(hostname)
     }
   }
 
