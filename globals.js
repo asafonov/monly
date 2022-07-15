2843fec69848d913f9c655e5372ccbc7e562d938
asafonov <me@asafonov.org>
2022-07-15 15:49:52 +0300

    version bump

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
 window.asafonov = {}
-window.asafonov.version = '1.20'
+window.asafonov.version = '1.21'
 window.asafonov.utils = new Utils()
 window.asafonov.messageBus = new MessageBus()
 window.asafonov.events = {
