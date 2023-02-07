fa26307ea3a89430700fe026be19d1536a4eca53
asafonov <me@asafonov.org>
2023-02-07 17:00:14 +0100

    reports p.2

--- view/reports.js
+++ view/reports.js
@@ -3,7 +3,7 @@ class ReportsView {
   constructor() {
     this.model = new Reports()
     this.controller = new ReportsController()
-    this.circleLen = 251
+    this.circleLen = 126
     this.circleDeg = 360
     this.initAvailableReports()
   }
@@ -38,8 +38,6 @@ class ReportsView {
     this.model.destroy()
     this.model = new Reports(y, m)
     this.show()
-    this.togglePopup()
-    this.active.innerHTML = this.options.querySelector(`.m${m}`).innerHTML + ' ' + y
   }
 
   clearExistingItems() {
@@ -54,14 +52,15 @@ class ReportsView {
   }
 
   showIncome() {
-    this.element = document.querySelector('.income.monly-circle')
-    this.donutElement = this.element.querySelector('.donut.chart')
+    this.element = document.querySelector('.monly-income')
+    this.donutElement = this.element.querySelector('.chart_donut')
+    this.legendElement = this.element.querySelector('.chart_legend')
     this.showChart(i => i < 0)
   }
 
   show() {
     this.showExpenses()
-    //this.showIncome()
+    this.showIncome()
   }
 
   showChart (proceedFunction) {
@@ -70,6 +69,12 @@ class ReportsView {
 
     this.model.build()
     const data = this.model.getItem(0)
+
+    if (! data) {
+      this.donutElement.innerHTML += `<h2>No data</h2>`
+      return
+    }
+
     const subtotal = Object.values(data).filter(proceedFunction).reduce((accumulator, currentValue) => accumulator + currentValue, 0)
     const total = Math.abs(subtotal)
     let degOffset = -90
@@ -77,12 +82,13 @@ class ReportsView {
     const keys = Object.keys(data)
     keys.sort((a, b) => Math.abs(data[a]) - Math.abs(data[b]))
 
-    for (let item of keys) {
+    for (let i = 0; i < keys.length; ++i) {
+      const item = keys[i]
+
       if (! proceedFunction(data[item])) continue
 
       const value = Math.abs(data[item])
       const lineLen = value / total * this.circleLen
-      lengthOffset += lineLen
       const deg = value / total * this.circleDeg
       const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
       svg.setAttribute('viewBox', '0 0 100 100')
@@ -90,17 +96,18 @@ class ReportsView {
       const circle = document.createElement('circle')
       circle.style.strokeDashoffset = lengthOffset
       degOffset += deg
+      lengthOffset += lineLen
       svg.appendChild(circle)
       this.donutElement.appendChild(svg)
-      alert(this.donutElement.innerHTML)
 
-      /*const itemDiv = document.createElement('div')
-      itemDiv.className = 'item'
-      itemDiv.innerHTML = `<div><span class="bullet slice_${i}"></span>${item}</div>`
+      const itemDiv = document.createElement('div')
       const displayMoney = asafonov.utils.displayMoney(value)
-      itemDiv.innerHTML += `<div class="number">${displayMoney}</div>`
-      this.donutElement.after(itemDiv)*/
+      const underline = i < keys.length - 1 ? '<div class="underline"></div>' : ''
+      itemDiv.className = 'legend_row'
+      itemDiv.innerHTML = `<div class="section_row chart_row"><p class="dot">${item}</p><p class="number">${displayMoney}</p></div>${underline}`
+      this.legendElement.appendChild(itemDiv)
     }
+    alert(this.donutElement.innerHTML)
 
     this.donutElement.innerHTML += `<h2>${asafonov.utils.displayMoney(total)}</h2>`
   }
